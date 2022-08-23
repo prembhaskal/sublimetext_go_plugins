f6c0538fca10ae89efa962f83b22595da46643a5
bhaskal <premkumar.bhaskal@nokia.com>
2022-08-23 21:31:03 +0530

    added first version of the plugin file

---
+++ gofumports/GoFumports.py
@@ -0,0 +1,14 @@
+import sublime
+import sublime_plugin
+import subprocess
+
+
+class GoFumportsCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        filename = self.view.file_name()
+        sublime.status_message("got full path")
+        print("full name of file is " + filename)
+        subprocess.run(["gofumports", "-w", filename], capture_output=True)
+
+    def is_enabled(self):
+        return bool(self.view.file_name() and len(self.view.file_name()) > 0)
